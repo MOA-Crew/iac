94b40a9aee32eb1e36a5ef0ce89f13e4cb7ddaee
yeounhyeok <yeounhyeok@gmail.com>
2026-05-19 02:09:54 +0900

    feat: open ingress port for http protocol

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -59,6 +59,22 @@ resource "aws_security_group" "this" {
     cidr_blocks = [var.ssh_allowed_cidr]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "All outbound"
     from_port   = 0
